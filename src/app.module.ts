1bce75daab9d9af1aad4d98678ee10195075f9af
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-08-31 21:02:30 +0530

    app module added

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    BookModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
