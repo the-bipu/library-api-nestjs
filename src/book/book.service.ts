e18de42dadfefd539f5b5eec2cbbf02bd47b293e
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-09-01 02:48:09 +0530

    update services

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Book } from './schemas/book.schema';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+    constructor(
+        @InjectModel(Book.name)
+        private bookModel: mongoose.Model<Book>
+    ) {}
+
+    async findAll(): Promise<Book[]> {
+        const books = await this.bookModel.find()
+        return books
+    }
+}
