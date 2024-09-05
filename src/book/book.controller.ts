4881a0e52a2d85adce215d1f6677c1704dfdcc7f
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-09-05 23:56:38 +0530

    create dto

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('book')
 export class BookController {
@@ -14,8 +16,34 @@ export class BookController {
     @Post()
     async createBook(
         @Body()
-        book,
+        book: CreateBookDto,
     ): Promise<Book> {
         return this.bookService.create(book);
+    }
+
+    @Get(':id')
+    async getBook(
+        @Param('id')
+        id: string
+    ): Promise<Book> {
+        return this.bookService.findById(id);
+    }
+
+    @Put(':id')
+    async updateBook(
+        @Param('id')
+        id: string,
+        @Body()
+        book: UpdateBookDto,
+    ): Promise<Book> {
+        return this.bookService.updateById(id, book);
+    }
+
+    @Delete(':id')
+    async deleteBook(
+        @Param('id')
+        id: string
+    ): Promise<Book> {
+        return this.bookService.deleteById(id);
     }
 }
