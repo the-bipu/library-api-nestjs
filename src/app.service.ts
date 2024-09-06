a4088c7d14caacffeca488547c0b17236d452b8a
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-09-06 15:15:01 +0530

    added html file
    added get route in the documentation

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const htmlFilePath = join(__dirname, '..', 'views', 'index.html');
+    const htmlContent = readFileSync(htmlFilePath, 'utf-8');
+    return htmlContent;
   }
 }
